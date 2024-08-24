74ceac8cc2664fa714222301ee5e4d1293e11994
beom324 <scarlet2010@nate.com>
2024-08-24 14:31:01 +0900

    GET Param Text

---
+++ src/main/java/hello/servlet/basic/request/RequestBodyStringServlet.java
@@ -0,0 +1,27 @@
+package hello.servlet.basic.request;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.util.StreamUtils;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet(name = "RequestBodyStringServlet", urlPatterns = "/request-body-string")
+public class RequestBodyStringServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletInputStream inputStream = req.getInputStream();
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);//바이트를 문자열로 바꿀때는 인코딩 정보를 알려주어야함
+
+        System.out.println("messageBody = " + messageBody);
+
+        resp.getWriter().write("ok");
+
+
+    }
+}
